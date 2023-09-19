3cf19766e523f96331dd41e271806776ea7eb283
Gustavo Torregrosa Costa <gustavo@Gustavos-MacBook-Air.local>
2023-09-19 11:36:48 -0300

    parcial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started 5 - by editing&nbsp;
+            Get started 10 - by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
